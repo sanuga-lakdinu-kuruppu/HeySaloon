8e3ff02510e27cf9f0c1c04c00dd723b24640acd
Sanuga Kuruppu <128165236+sanuga-lakdinu-kuruppu@users.noreply.github.com>
2025-03-29 15:52:48 +0530

    stylists search is done

---
+++ HeySaloon/Home/View/SearchItemView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct SearchItemView: View {
+
+    var titleText: String
+    var subtitleText: String
+    var isThisLocation: Bool = true
+    @State var screenwidth: CGFloat = UIScreen.main.bounds.width
+
+    var body: some View {
+
+        HStack(spacing: 16) {
+            VStack {
+                Image(
+                    systemName: isThisLocation ? "scope" : "clock.fill"
+                )
+                .resizable()
+                .frame(
+                    width: screenwidth * 0.06, height: screenwidth * 0.06)
+            }
+            .foregroundColor(Color("MainBackgroundColor"))
+            .frame(width: screenwidth * 0.12, height: screenwidth * 0.12)
+            .background(isThisLocation ? .white : .accent)
+            .cornerRadius(50)
+
+            VStack(alignment: .leading) {
+                CalloutTextView(
+                    text:
+                        titleText
+                )
+                CaptionTextView(text: subtitleText)
+                CommonDividerView()
+            }
+            Spacer()
+        }
+        .padding(.top, 16)
+    }
+
+}
+
+#Preview {
+    SearchItemView(
+        titleText: "fjadslk",
+        subtitleText: "fasjdl",
+        isThisLocation: true
+    )
+}
